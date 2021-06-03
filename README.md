279030f552af31cd84ad5597c9553704c75ff7ba
zhanghx0905 <731931282@qq.com>
2021-06-03 21:37:34 +0800

    reorganize files and update report

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # my-cholmod
 
+Python 下大规模稀疏矩阵的 Cholesky 分解算法
+
+A Cython wrapper for the SuiteSparse CHOLMOD library for sparse Cholesky decomposition
+
 ### References
 
 [SuiteSparse](https://github.com/DrTimothyAldenDavis/SuiteSparse)
 
 [scikit-sparse](https://github.com/scikit-sparse/scikit-sparse)
+
--- tests/test.sh
+++ tests/test.sh
@@ -5,12 +5,11 @@ python3 setup.py build_ext -t ../tests -b ../tests
 cd ../tests
 make
 
-# pytest-3  # 测试正确性
-# 测试性能
-# python3 test_performance.py
-# echo
-# ./cholmod_c_test ./test_data/ted_B.mtx
-# ./cholmod_c_test ./test_data/s3rmt3m3.mtx
-# ./cholmod_c_test ./test_data/thermomech_dM.mtx
-# ./cholmod_c_test ./test_data/parabolic_fem.mtx
+pytest-3                    # 测试正确性
+python3 test_performance.py # 测试性能
+echo
+./cholmod_c_test ./test_data/ted_B.mtx
+./cholmod_c_test ./test_data/s3rmt3m3.mtx
+./cholmod_c_test ./test_data/thermomech_dM.mtx
+./cholmod_c_test ./test_data/parabolic_fem.mtx
 make clean
